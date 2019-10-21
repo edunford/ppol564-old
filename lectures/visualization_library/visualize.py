c38c26c5a26a09e76842aa8e5aa78de789a1ab3a
edunford <eric.dunford@georgetown.edu>
2019-10-21 12:53:25 -0400

    Basically reverted color scheme back to original

--- lectures/visualization_library/visualize.py
+++ lectures/visualization_library/visualize.py
@@ -60,12 +60,12 @@ class LinearAlgebra:
 
             # Plot grid lines
             for i in range(-(extent),(extent)+1):
-                self.grid_line(np.array([0,1]),scale=extent,y_nudge=i,alpha=.5,color="steelblue")
-                self.grid_line(np.array([1,0]),scale=extent,x_nudge=i,alpha=.5,color="steelblue")
+                self.grid_line(np.array([0,1]),scale=extent,y_nudge=i,alpha=.5,color="black")
+                self.grid_line(np.array([1,0]),scale=extent,x_nudge=i,alpha=.5,color="black")
 
         else:
-            self.grid_line(np.array([1,0]),scale=extent,lwd=1.5,color="steelblue")
-            self.grid_line(np.array([0,1]),scale=extent,lwd=1.5,color="steelblue")
+            self.grid_line(np.array([1,0]),scale=extent,lwd=1.5)
+            self.grid_line(np.array([0,1]),scale=extent,lwd=1.5)
 
 
 
